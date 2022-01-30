2d0bf039c61cba8e72ff7da493ef22f339c50182
A. Sivault <81822795+Sivarth91@users.noreply.github.com>
2022-01-30 13:48:28 +0100

    guest number game

---
+++ guestNumber.php
@@ -0,0 +1,23 @@
+<?php 
+
+$isNumber = false;
+
+echo "You need to find a number between 0 and 150 \n";
+// 
+
+while (!$isNumber) {
+    $response = readline("Enter a number:");
+
+    if (ctype_digit($response)) {
+        $isNumber = true;
+        if ($response < 83) {
+            echo "Your number is too small";
+        } elseif ($response > 83) {
+                echo "Your number is too large";
+        } else {
+            echo "Bravo, you have found the number";
+        }
+    } else {
+        echo "You need to give a number ! \n";
+    }
+}
